e33a28d676a3196b861320c5fbab1373b2a590e4
Chiara Fragomeli <chiara.fragomeli@student.unisi.it>
2023-04-27 12:45:48 +0200

    eclipse added

--- dev/Hello.java
+++ dev/Hello.java
@@ -2,6 +2,6 @@
 // Il nome della classe deve essere uguale al nome del file
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Chiara!");
+        System.out.println("Hello!");
     }
 }
